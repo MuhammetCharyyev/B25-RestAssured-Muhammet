101714cacc2144571047399c1562a7c5a0b4a34f
Muhammet <muhammedabuchary@gmail.com>
2022-04-25 15:14:55 -0500

    API_day5

--- src/test/java/com/cydeo/utilities/SpartanTestBase.java
+++ src/test/java/com/cydeo/utilities/SpartanTestBase.java
@@ -1,7 +1,10 @@
 package com.cydeo.utilities;
 
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+
+import java.sql.*;
 
 public class SpartanTestBase {
     @BeforeAll
@@ -9,5 +12,16 @@ public class SpartanTestBase {
 
         RestAssured.baseURI = "http://18.215.145.203:8000";
         //added in Before all to keep this before all tests
+
+        String url_sp = "jdbc:oracle:thin:@18.215.145.203:1521:XE";
+        String username_sp = "SP";
+        String password_sp = "SP";
+
+        DB_Utils.createConnection(url_sp,username_sp,password_sp);
+    }
+
+    @AfterAll
+    public static void close(){
+        DB_Utils.destroy();
     }
 }
