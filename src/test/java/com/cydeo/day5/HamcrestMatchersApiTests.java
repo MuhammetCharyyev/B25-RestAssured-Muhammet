02abf1d5f74fcf27c1bff837736084142a5501f6
Muhammet <muhammedabuchary@gmail.com>
2022-04-25 13:20:27 -0500

    API_day5

--- src/test/java/com/cydeo/day5/HamcrestMatchersApiTests.java
+++ src/test/java/com/cydeo/day5/HamcrestMatchersApiTests.java
@@ -37,12 +37,52 @@ public class HamcrestMatchersApiTests {
                 .and()
                 .contentType("application/json")
                 .and()
-                .body("id",is(15),
+                .body("id",is(15), //verify full body by id
                         "name",is("Meta"),
                         "gender",equalTo("Female"),
                         "phone",is(1938695106));
 
     }
+
+    @DisplayName("CBTraining Teacher request with chaining and matchers")
+    @Test
+    public void test2(){
+        given()
+                .accept(ContentType.JSON)
+                .and()
+                .pathParam("id",10423)
+                .when()
+                .get("http://api.cybertektraining.com/teacher/{id}")
+                .then()
+                .statusCode(200)
+                .and()
+                .contentType("application/json;charset=UTF-8")
+                .and()
+                .header("Content-Encoding","gzip")//verification header content
+                .and()
+                .header("Date",notNullValue())
+                //as date is dynamic data then we may verify only if header is not null
+                .body("teachers[0].firstName",is("Alexander"),
+                        "teachers[0].lastName",is("Syrup"),
+                        "teachers[0].gender",equalTo("male"));
+        //we put index [0] because 'teacher' has this body inside by this index#
+
+    }
+
+    @Test
+    public void test3(){
+        //verify "Candi","Alexander","Francesca" inside the all teachers
+        given()
+                .accept(ContentType.JSON)
+                .when()
+                .get("http://api.cybertektraining.com/teacher/all")
+                .then()
+                .statusCode(200)
+                .and()
+                .body("teachers.firstName",hasItems("Candi","Alexander","Francesca"));
+        //check all 'teacher' first names by consisting specific names (hasItems)
+    }
+
 
 
 }
---
+++ src/test/java/com/cydeo/day5/HrHamcrestTests.java
@@ -0,0 +1,78 @@
+package com.cydeo.day5;
+
+import com.cydeo.utilities.HrTestBase;
+
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+
+public class HrHamcrestTests extends HrTestBase {
+
+    @DisplayName("GET request to Employees IT_PROG endpoint and chaining")
+    @Test
+    public void test1() {
+        //TASK
+        //send a get request to emplyoees endpoint with query parameter job_id IT_PROG
+        //verify status code and content type
+        //verify each job_id is IT_PROG
+        //verify first names are .... (find proper method to check list against list)
+        //verify emails without checking order (provide emails in different order,
+        // just make sure it has same emails)
+
+        //expected names
+        List<String> names = Arrays.asList("Alexander", "Bruce", "David", "Valli", "Diana");
+
+        given()
+                .accept(ContentType.JSON)
+                .and()
+                .queryParam("q","{\"job_id\":\"IT_PROG\"}")
+                .when()
+                .get("/employees")
+                .then()
+                .statusCode(200)
+                .and()
+                .body("items.job_id", everyItem(equalTo("IT_PROG")))
+                //checking all job_id in 'items' and look for every item equals to IT_PROG
+                .body("items.salary" , everyItem(greaterThan(5000)))
+                //checking all salary in 'items' and look for every item greater 5000
+                .body("items.first_name", equalTo(names))
+                //equalTo looking for exact matching of out List
+                .body("items.email", containsInAnyOrder())
+                .body("items.email",containsInAnyOrder
+                        ("DAUSTIN","AHUNOLD","BERNST","VPATABAL","DLORENTZ"));
+
+    }
+
+    @Test
+    public void test2(){
+        //we want to chain with hamcrest and also get the response object.
+        JsonPath jsonPath = given()
+                .accept(ContentType.JSON)
+                .and()
+                .queryParam("q","{\"job_id\":\"IT_PROG\"}")
+                .when()
+                .get("/employees")
+                .then()
+                .statusCode(200)
+                .body("items.job_id",everyItem(equalTo("IT_PROG")))
+                .extract().response().jsonPath();
+
+//extract() --> method that allow us to get response object after we use then() method
+//it used when we want to save our variable and use it later as below, for example
+
+        //assert that we have only 5 firstnames
+        assertThat(jsonPath.getList("items.first_name"),hasSize(5));
+
+
+
+    }
+
+}
