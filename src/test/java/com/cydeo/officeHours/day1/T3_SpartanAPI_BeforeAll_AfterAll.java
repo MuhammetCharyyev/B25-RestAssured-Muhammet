6ec23cf42a958a48b9f91052c63ba020d475e672
Muhammet <muhammedabuchary@gmail.com>
2022-04-27 16:15:18 -0500

    OfficeHours_API_day1

--- src/test/java/com/cydeo/officeHours/day1/T3_SpartanAPI_BeforeAll_AfterAll.java
+++ src/test/java/com/cydeo/officeHours/day1/T3_SpartanAPI_BeforeAll_AfterAll.java
@@ -1,6 +1,15 @@
 package com.cydeo.officeHours.day1;
 
+import io.restassured.RestAssured;
+
+import static io.restassured.RestAssured.*;
+
+import io.restassured.response.Response;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 public class T3_SpartanAPI_BeforeAll_AfterAll {
 
@@ -18,7 +27,18 @@ public class T3_SpartanAPI_BeforeAll_AfterAll {
      *      - Get me third spartan name
      *      - Get me all spartan name
      */
+//create BeforeAll method
+    @BeforeAll
+    public static void init() {
+        baseURI = "http://3.86.82.161:8000";
+        basePath = "/api";
 
+    }
+
+    @AfterAll
+    public static void resetApi() {
+        reset();
+    }
     @Test
     public void test() {
 
