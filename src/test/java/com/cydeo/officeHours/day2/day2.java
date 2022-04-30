a29de7f09ee03cb09b5cda293459b98b669aff47
Muhammet <muhammedabuchary@gmail.com>
2022-04-30 09:16:29 -0500

    AvengersClass_day2

---
+++ src/test/java/com/cydeo/officeHours/day2/day2.java
@@ -0,0 +1,18 @@
+package com.cydeo.officeHours.day2;
+
+public class day2 {
+
+    /**
+     *   1. Send request to get all Spartans  and get me the status code
+     *     2. get me headers for above request
+     *     3. get me header value for Content-Type and Date
+     *     4. get me all Spartans printed
+     *     5. get me 7th spartan (2 different ways)
+     *     6. get me Spartans name Allen
+     *     7. get me all male Spartans
+     *     8. get me the spartan that female and name has z
+     *     9. get name, gender, phone separately of No.15 Spartans
+     *     10. get me all Spartans and get 10st Spartan's name, 20st Spartans gender, 30st Spartans id.
+     */
+
+}
